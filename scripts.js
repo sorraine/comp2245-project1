867f14a169bde56441af6ac86f6c1a9a5a6c4aa2
Kalifa Roberts <kalifasroberts@gmail.com>
2022-11-02 19:50:48 -0400

    Completed javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var emailaddress = document.querySelector('input[type="email"]');
+    var  btn = document.querySelector('button[type="submit"]');
+    var messages = document.querySelector(".message");
+
+    
+
+    btn.addEventListener("click", function (event){
+        event.preventDefault();
+
+        var txt = emailaddress.value;
+
+        if (validateEmail(txt)) {
+
+            messages.innerHTML = ` ${txt} has been added to our mailing list!`;
+        
+        } else {
+
+            messages.innerHTML = `Re-enter a valid email address.`;
+            
+        }
+
+    });
+    
+})
+
+function validateEmail(emailaddress) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(emailaddress);
+  }
